7a6ded12bc702edd2113b25751822613b0db2fbe
ctocicd <ctocicd@intuit.com>
2021-03-11 11:10:00 -0800

    POST route with JSON example

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -3,6 +3,8 @@ package example
 import zio.App
 import zio.ZEnv
 import zio.ZIO
+import zio.json._
+
 import zhttp.TcpServer
 import zhttp.HttpRouter
 import zhttp.MyLogging.MyLogging
@@ -13,6 +15,15 @@ import zhttp.dsl._
 import zhttp.Response
 import zhttp.Method._
 
+
+object UserRecord {
+  implicit val decoder: JsonDecoder[UserRecord] = DeriveJsonDecoder.gen[UserRecord]
+  implicit val encoder: JsonEncoder[UserRecord] = DeriveJsonEncoder.gen[UserRecord]
+}
+case class UserRecord(val uid: String )
+
+
+
 object ServerExample extends zio.App {
 
   def run(args: List[String]) = {
@@ -21,7 +32,15 @@ object ServerExample extends zio.App {
     val r =  HttpRoutes.of {
        case GET -> Root / "health" =>
         ZIO(Response.Ok.asTextBody("Health Check Ok"))
+
+       case req @ POST -> Root / "test" =>
+         for {
+           rec <- ZIO( req.fromJSON[UserRecord] )
+           _   <- MyLogging.info("my_application", "UID received: " + rec.uid )
+         } yield( Response.Ok.asTextBody( "OK " + rec.uid ) )
     }
+
+
 
     type MyEnv = MyLogging
 
@@ -36,7 +55,9 @@ object ServerExample extends zio.App {
     myHttp.SERVER_PORT = 8080
 
 
-    val logger_L = MyLogging.make(("console" -> LogLevel.Trace), ("access" -> LogLevel.Info))
+    val logger_L = MyLogging.make( ("console" -> LogLevel.Trace), 
+                                   ("access" -> LogLevel.Info), 
+                                   ( "my_application" -> LogLevel.Info) )
 
     myHttp.run( myHttpRouter.route ).provideSomeLayer[ZEnv](logger_L).exitCode
 
